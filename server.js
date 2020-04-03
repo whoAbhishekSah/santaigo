3f9fc4f89e66340e74cf0298e591901d3abeaa12
Abhishek Sah <abhishek.sah@go-jek.com>
2020-04-03 12:43:11 +0530

    [Abhishek] Send message to slack

---
+++ server.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+
+let submissionCount = 0;
+
+const KEY = process.env.WEBHOOK_KEY
+
+axios
+  .get("https://codeforces.com/api/user.status?handle=anxmukul")
+  .then(response => {
+    const submissions = response.data.result;
+    if (submissions.length != submissionCount) {
+      submissionCount = submissions.length;
+      const lastSubmission = submissions[submissions.length - 1];
+      const problemName = lastSubmission.problem.name;
+      const language = lastSubmission.programmingLanguage;
+      const result = lastSubmission.verdict;
+      let slackMessage;
+      if (result === "OK") {
+        slackMessage = `Submission for *${problemName}* in _${language}_ passed :tada:`;
+      } else {
+        slackMessage = `Submission for *${problemName}* in _${language}_ failed with \`${result}\` :ghost:`;
+      }
+      axios
+        .post(
+          `https://hooks.slack.com/services/${KEY}`,
+          JSON.stringify({ text: slackMessage }),
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded"
+            }
+          }
+        )
+        .then(function() {
+          console.log("Santiago sent a message");
+        })
+        .catch(function(error) {
+          console.log("Santiago encountered an error", error);
+        });
+    }
+  })
+  .catch(error => {
+    console.log(error);
+  });
