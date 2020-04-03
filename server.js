a15aba5ebf980234584825b56bff6e07bbfd9c4e
Abhishek Sah <abhishek.sah@go-jek.com>
2020-04-03 15:06:17 +0530

    [Abhishek] Add express server

--- server.js
+++ server.js
@@ -1,47 +1,57 @@
 const axios = require("axios");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
 let submissionCount = 0;
 
-const KEY = process.env.WEBHOOK_KEY
+const KEY = process.env.WEBHOOK_KEY;
 
-function f(){
+function f() {
   axios
-  .get("https://codeforces.com/api/user.status?handle=anxmukul")
-  .then(response => {
-    const submissions = response.data.result;
-    if (submissions.length != submissionCount) {
-      submissionCount = submissions.length;
-      const lastSubmission = submissions[submissions.length - 1];
-      const problemName = lastSubmission.problem.name;
-      const language = lastSubmission.programmingLanguage;
-      const result = lastSubmission.verdict;
-      let slackMessage;
-      if (result === "OK") {
-        slackMessage = `Submission for *${problemName}* in _${language}_ passed :tada:`;
-      } else {
-        slackMessage = `Submission for *${problemName}* in _${language}_ failed with \`${result}\` :ghost:`;
-      }
-      axios
-        .post(
-          `https://hooks.slack.com/services/${KEY}`,
-          JSON.stringify({ text: slackMessage }),
-          {
-            headers: {
-              "Content-Type": "application/x-www-form-urlencoded"
+    .get("https://codeforces.com/api/user.status?handle=anxmukul")
+    .then(response => {
+      const submissions = response.data.result;
+      if (submissions.length != submissionCount) {
+        submissionCount = submissions.length;
+        const lastSubmission = submissions[submissions.length - 1];
+        const problemName = lastSubmission.problem.name;
+        const language = lastSubmission.programmingLanguage;
+        const result = lastSubmission.verdict;
+        let slackMessage;
+        if (result === "OK") {
+          slackMessage = `Submission for *${problemName}* in _${language}_ passed :tada:`;
+        } else {
+          slackMessage = `Submission for *${problemName}* in _${language}_ failed with \`${result}\` :ghost:`;
+        }
+        axios
+          .post(
+            `https://hooks.slack.com/services/${KEY}`,
+            JSON.stringify({ text: slackMessage }),
+            {
+              headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+              }
             }
-          }
-        )
-        .then(function() {
-          console.log("Santiago sent a message");
-        })
-        .catch(function(error) {
-          console.log("Santiago encountered an error", error);
-        });
-    }
-  })
-  .catch(error => {
-    console.log(error);
-  });
+          )
+          .then(function() {
+            console.log("Santiago sent a message");
+          })
+          .catch(function(error) {
+            console.log("Santiago encountered an error", error);
+          });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
+app.get("/", (req, res) => res.send("Hello World!"));
 
-setInterval(function(){ f()},60000)
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
+
+setInterval(function() {
+  f();
+}, 60000);
